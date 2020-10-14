a8a66231bf33e63b322842bad3303d4d94cab32b
Mauricio Ramirez <mfernande@finishline.com>
2020-10-14 14:38:41 -0600

    Initial set up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import sheepManagerReducer from './store/reducers/sheepManagerReducer'
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const rootReducer = combineReducers({
+  sheepManager: sheepManagerReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Provider>
 );
 
+ReactDOM.render(app, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ src/store/reducers/sheepManagerReducer.js
@@ -0,0 +1,15 @@
+import * as actionTypes from '../actions/actionTypes';
+
+
+const initialState = {
+    sheeps: null
+
+}
+
+const reducer = ( state = initialState, action) => {
+    switch (action) {
+        default: return state;
+    }
+}
+
+export default reducer;
