7ac09a54b1e0e6eaff0bd8445d92ef3b96932214
Mauricio Ramirez <mfernande@finishline.com>
2021-01-10 21:20:06 -0600

    Fixing ta  tailwindcss dependencies

--- src/components/Navigation/Sidebar/Sidebar.js
+++ src/components/Navigation/Sidebar/Sidebar.js
@@ -8,7 +8,7 @@ const sidebar = () => {
 
     return (
         <Aux>
-            <div className="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-secondary overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
+            <div className="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-secondary overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 sm:hidden ">
                 <div className="flex items-center justify-center mt-8">
                     <div className="flex items-center">
                         <img className="h-10 w-10" src={logo}></img>
@@ -20,7 +20,7 @@ const sidebar = () => {
                     <NavigationItems></NavigationItems>
                 </nav>
             </div>
-            <div className="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden block"></div>
+            <div className="fixed z-20 inset-0 bg-black opacity-50 transition-opacity hidden block"></div>
         </Aux>
 
     )
